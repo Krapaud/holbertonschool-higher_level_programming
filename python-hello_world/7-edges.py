477c99872d90708e6e01ce8f20da757846f4f11e
Krapaud <mur.mickael@gmail.com>
2025-09-01 18:58:40 +0200

    Ajout des fichiers 7-edges.py et 8-concat_edges.py pour des exercices de découpage de chaînes

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for string slicing exercise."""
+
+WORD = "Holberton"
+WORD_FIRST_3 = WORD[:3]
+WORD_LAST_2 = WORD[-2:]
+MIDDLE_WORD = WORD[1:-1]
+print(f"First 3 letters: {WORD_FIRST_3}")
+print(f"Last 2 letters: {WORD_LAST_2}")
+print(f"Middle word: {MIDDLE_WORD}")
