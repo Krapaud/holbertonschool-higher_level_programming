27a1b9c683f459726bd51e6e79bb9b3984d11c22
Krapaud <mur.mickael@gmail.com>
2025-09-02 15:36:07 +0200

    Correction de la structure des conditions dans le script 1-last_digit.py pour améliorer la lisibilité et la logique.

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,8 @@ elif number < 0:
 
 if last_digit > 5:
     print(f"Last digit of {number} is {last_digit} greater than 5")
-if last_digit == 0:
+elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
-if last_digit < 6 and last_digit != 0:
+elif last_digit < 6 and last_digit != 0:
     print(f"Last digit of {number} is {last_digit} and is less than 6 "
           f"and not 0")
