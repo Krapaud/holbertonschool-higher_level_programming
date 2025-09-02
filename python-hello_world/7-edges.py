a82ccf0359b3dfc390ff45011671b12efce0a7db
Krapaud <mur.mickael@gmail.com>
2025-09-02 09:43:31 +0200

    Changement des noms de variables en minuscules pour respecter les conventions de style Python

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Module for string slicing exercise."""
 
-WORD = "Holberton"
-WORD_FIRST_3 = WORD[:3]
-WORD_LAST_2 = WORD[-2:]
-MIDDLE_WORD = WORD[1:-1]
-print(f"First 3 letters: {WORD_FIRST_3}")
-print(f"Last 2 letters: {WORD_LAST_2}")
-print(f"Middle word: {MIDDLE_WORD}")
+word = "Holberton"
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
