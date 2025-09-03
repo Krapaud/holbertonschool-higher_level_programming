40956ba52f7f8d2da86642e15165a7a60db0a660
Krapaud <mur.mickael@gmail.com>
2025-09-03 18:50:21 +0200

    Remove trailing spaces in print statements for FizzBuzz function

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,12 +2,12 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print("{}".format("FizzBuzz"), end=" ")
+            print("{}".format("FizzBuzz"), end="")
         elif i % 3 == 0:
-            print("{}".format("Fizz"), end=" ")
+            print("{}".format("Fizz"), end="")
         elif i % 5 == 0:
-            print("{}".format("Buzz"), end=" ")
+            print("{}".format("Buzz"), end="")
         else:
-            print("{}".format(i), end=" ")
+            print("{}".format(i), end="")
         if i != 100:
             print("{}".format(" "), end="")
