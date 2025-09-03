08d2580c8c914cf49285ec07b21c789764244b0a
Krapaud <mur.mickael@gmail.com>
2025-09-03 15:25:16 +0200

    Add islower function and main script to check character case

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def islower(c):
+    if c.islower:
+        print("{}: is lower".format(c))
+    elif c.isupper():
+        print("{}: is upper".format(c))
+    else:
+        print("{}:")
