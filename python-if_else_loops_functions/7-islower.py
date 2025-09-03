e683823d9c5dea34f57103c7a4953541c7c5921b
Krapaud <mur.mickael@gmail.com>
2025-09-03 15:35:04 +0200

    Refactor islower function to remove print statements and return boolean value

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if c.islower:
-        print("{}: is lower".format(c))
-    elif c.isupper():
-        print("{}: is upper".format(c))
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
     else:
-        print("{}:")
+        return False
