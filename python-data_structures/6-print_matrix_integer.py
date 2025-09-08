accff09a9e6af1b479bdd3879b41548db18c3df1
Krapaud <mur.mickael@gmail.com>
2025-09-08 14:31:34 +0200

    Refactor print_matrix_integer function to simplify matrix printing logic

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
+
 def print_matrix_integer(matrix=[[]]):
-    if matrix is not None:
-        for i in matrix:
-            for j in i:
-                print("{:d}".format(j), end=" ")
-            print()
+    for j in matrix:
+        print(" ".join("{:d}".format(i) for i in j))
