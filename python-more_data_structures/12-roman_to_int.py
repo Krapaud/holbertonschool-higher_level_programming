2b625f222e0d900b3cfa8e1fda1d727c83dec45e
Krapaud <mur.mickael@gmail.com>
2025-09-09 16:54:21 +0200

    Add roman_to_int function and test script for Roman numeral conversion

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    valeur = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if not roman_string:
+        return 0
+    sum = 0
+    prev = 0
+    for char in reversed(roman_string):
+        current = valeur.get(char, 0)
+        if current < prev:
+            sum -= current
+        else:
+            sum += current
+        prev = current
+    return sum
