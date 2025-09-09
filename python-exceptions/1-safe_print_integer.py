2425b3db15b2d8f84e3b70eb5b2561b5ed497570
Krapaud <mur.mickael@gmail.com>
2025-09-09 19:08:32 +0200

    Refactor safe_print_integer function to remove docstring and streamline error handling

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    """
-    Prints an integer with "{:d}".format().
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
 
-    Args:
-        value: can be any type (integer, string, etc.)
-
-    Returns:
-        True if value has been correctly printed (it means the value is an integer)
-        Otherwise, returns False
-    """
-    # Your implementation here
-    pass
+        return False
