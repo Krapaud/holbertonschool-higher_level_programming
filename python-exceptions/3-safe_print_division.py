40771f0a194ecc18104cc731e79b5ab28b6dc3ef
Krapaud <mur.mickael@gmail.com>
2025-09-09 20:14:03 +0200

    Remove print statement from safe_print_division function to clean up output

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,6 @@ def safe_print_division(a, b):
     result = None
     try:
         result = a / b
-        print("{:d} / {:d} = {}".format(a, b, result))
     except ZeroDivisionError:
         return None
     finally:
