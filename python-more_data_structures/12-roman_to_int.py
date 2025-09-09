8acad018e6c6a717afd93d6a68863dd3bd95c87e
Krapaud <mur.mickael@gmail.com>
2025-09-09 17:08:23 +0200

    Fix input validation in roman_to_int function to check for None and non-string types

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     valeur = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if not roman_string:
+    if roman_string is None or type(roman_string) is not str:
         return 0
     sum = 0
     prev = 0
