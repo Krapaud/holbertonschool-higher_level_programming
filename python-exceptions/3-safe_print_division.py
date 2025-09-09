89ce9933e2dfe40ae7d1229244a1cb73b5725271
Krapaud <mur.mickael@gmail.com>
2025-09-09 20:07:21 +0200

    Implement safe_print_division function to handle division and print results

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    """
-    Divides 2 integers and prints the result.
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The value of the division, otherwise: None
-    """
-    # Your implementation here
-    pass
+    result = None
+    try:
+        result = a / b
+        print("{:d} / {:d} = {}".format(a, b, result))
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
+    print()
