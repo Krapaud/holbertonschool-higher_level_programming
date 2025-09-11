465b306805337e1327926e130362f0e07a5e7cb1
Krapaud <mur.mickael@gmail.com>
2025-09-11 19:54:22 +0200

    Implement print_square function with error handling and add corresponding tests

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that prints a square with the character #.
+
+This module contains a function that prints a square.
+"""
+
+
+def print_square(size):
+    """Prints a square with the character #.
+
+    Args:
+        size: The size length of the square
+
+    Raises:
+        TypeError: If size is not an integer
+        ValueError: If size is less than 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("a must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size is float and size < 0:
+        raise TypeError("size must be an integer")
+    if size == 0:
+        return
+    for i in range(size):
+        print("#" * size)
