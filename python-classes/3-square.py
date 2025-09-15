98a0295053686704196b30e296c945f1e7771d7c
Krapaud <mur.mickael@gmail.com>
2025-09-15 14:12:57 +0200

    Implement size validation and area calculation in Square class

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -4,6 +4,7 @@
 
 class Square:
     """Represent a square."""
+    __size = None
 
     def __init__(self, size=0):
         """Initialize a new Square.
@@ -11,8 +12,12 @@ class Square:
         Args:
             size (int): The size of the new square.
         """
-        pass
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
-        """Return the current area of the square."""
-        pass
+        """Retourne l'aire actuelle du carré."""
+        return self.__size * self.__size
