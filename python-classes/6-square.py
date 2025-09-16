675c36a66f7d797b0eea20818b9ec2c6d8535afa
Krapaud <mur.mickael@gmail.com>
2025-09-16 08:45:27 +0200

    Fix position setter validation in Square class to ensure it accepts a tuple of two integers

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -3,3 +3,46 @@
 
 
 class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Get the current size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Get/set the current position of the square."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, (tuple, list) or len(value)) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Print the square with the # character."""
+        if self.size == 0:
+            print("")
+        else:
+            for _ in range(self.position[1]):
+                print("")
+            for _ in range(self.size):
+                print(" " * self.position[0] + "#" * self.size)
