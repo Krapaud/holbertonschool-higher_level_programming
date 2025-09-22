5a8270aa1a1530dfdd4b99e4e70b0e22974b080c
Krapaud <mur.mickael@gmail.com>
2025-09-22 15:19:29 +0200

    Implement BaseGeometry and Rectangle classes with validation and area method

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class with area method and integer validator"""
+
+    def area(self):
+        """Raises an Exception with the message area() is not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Valide que value est un entier positif"""
+        if isinstance(value, bool) or not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Rectange class with herited by BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Initialize Rectangle with width and height"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
