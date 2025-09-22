40b2cb0e3964ce6a5ce8bb1155f4b3a7d814467d
Krapaud <mur.mickael@gmail.com>
2025-09-22 15:39:38 +0200

    Refactor BaseGeometry class to include area method and integer validator

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-"""Module for Rectangle class"""
-from pyclbr import Class
-from pydoc import classname
+"""Module for BaseGeometry class"""
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+class BaseGeometry:
+    """BaseGeometry class with area method and integer validator"""
+
+    def area(self):
+        """Raises an Exception with the message area() is not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Valide que value est un entier positif"""
+        if isinstance(value, bool) or not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
