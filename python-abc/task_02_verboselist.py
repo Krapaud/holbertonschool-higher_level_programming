d54456ff469f1e567d73477d2f38ad63eb0467df
Krapaud <mur.mickael@gmail.com>
2025-09-23 15:42:56 +0200

    Add implementation of VerboseList class with notification methods for list operations

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Module pour la classe VerboseList qui étend la liste Python
+"""
+
+
+class VerboseList(list):
+    """Classe qui étend list avec des notifications"""
+
+    def append(self, item):
+        """Ajouter un élément à la liste avec notification"""
+        super().append(item)
+        print("Add [{}] to the list.".format(item))
+
+    def extend(self, items):
+        """Étendre la liste avec notification"""
+        super().extend(items)
+        print("Extended the list with [{}] items.".format(len(items)))
+
+    def remove(self, item):
+        """Supprimer un élément de la liste avec notification"""
+        super().remove(item)
+        print("Removed [{}] from the list.".format(item))
+
+    def pop(self, index=-1):
+        """Supprimer et retourner un élément avec notification"""
+        item = super().pop(index)
+        print("Popped [{}] from the list.".format(item))
