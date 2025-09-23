96ab9f9b581ea17406e634328468852b78a3920c
Krapaud <mur.mickael@gmail.com>
2025-09-23 09:12:48 +0200

    Fix integer validation in BaseGeometry class to exclude boolean values

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -10,8 +10,8 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Valide que value est un entier positif"""
-        if type(value) is not int:
+        """Validates that value is a positive integer"""
+        if isinstance(value, bool) or not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
