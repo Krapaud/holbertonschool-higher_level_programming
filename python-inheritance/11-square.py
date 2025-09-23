4be4254203889222b74594b137687d4fff0f1115
Krapaud <mur.mickael@gmail.com>
2025-09-23 09:34:18 +0200

    Add Square class with area method and integer validation

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class with area method and integer validator"""
+
+    def area(self):
+        """Raises an Exception with the message area() is not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Valide que value est un entier positif"""
+        if isinstance(value, bool) or not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class inherited from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Initialize Rectangle with width and height"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Calculate the area of the Rectangle.
+
+        Returns:
+            int: The area of the rectangle (width * height)
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the string representation of the Rectangle.
+
+        Returns:
+            str: A string representation using print_symbol characters.
+                 Returns empty string if width or height is 0.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    """Square class that inherits from Rectangle"""
+
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Return the current area of the square."""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Return the string representation of the Rectangle.
+
+        Returns:
+            str: A string representation using print_symbol characters.
+                 Returns empty string if width or height is 0.
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
