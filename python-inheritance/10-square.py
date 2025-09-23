21b2ee58d699c445b535cc008b5b8878955b6b96
Krapaud <mur.mickael@gmail.com>
2025-09-23 09:30:04 +0200

    Add area method to Square class and validate size during initialization

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -52,4 +52,10 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Instantiation with size"""
+        self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Return the current area of the square."""
+        return self.__size * self.__size
