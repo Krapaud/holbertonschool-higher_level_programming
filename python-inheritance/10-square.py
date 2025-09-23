aae5dc3a992fe6d63fb562a9254cd4354067d83b
Krapaud <mur.mickael@gmail.com>
2025-09-23 09:29:12 +0200

    Remove redundant size validation and area method from Square class

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -52,10 +52,4 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Instantiation with size"""
-        self.integer_validator("size", size)
         super().__init__(size, size)
-        self.__size = size
-
-    def area(self):
-        """Return the current area of the square."""
-        return self.__size * self.__size
