ac391f01ad150a33f0b36236231ea04202bf7d52
Krapaud <mur.mickael@gmail.com>
2025-09-24 09:03:13 +0200

    Add implementation of CountedIterator class to count iterated elements

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module pour la classe CountedIterator
+"""
+
+
+class CountedIterator:
+    """Classe qui étend un itérateur en comptant les éléments parcourus"""
+
+    def __init__(self, iterable):
+        """Constructeur de CountedIterator"""
+        self._iterator = iter(iterable)
+        self._count = 0
+
+    def __next__(self):
+        """Méthode pour obtenir le prochain élément"""
+        try:
+            item = next(self._iterator)
+            self._count += 1
+            return item
+        except StopIteration:
+            raise
+
+    def get_count(self):
+        """Retourner le nombre d'éléments parcourus"""
+        return self._count
