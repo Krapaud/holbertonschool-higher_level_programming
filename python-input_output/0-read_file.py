9b39deda7b6b0ca4a0c303a6d3f80fed1eda3f66
Krapaud <mur.mickael@gmail.com>
2025-09-29 10:28:26 +0200

    Add functions for reading, writing, appending, and JSON conversion in file operations

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that contains a function to read a file"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout
+    
+    Args:
+        filename (str): The name of the file to read
+    """
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end="")
---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that contains a function to convert to JSON string"""
+import json
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object (string)
+    
+    Args:
+        my_obj: The object to convert to JSON string
+        
+    Returns:
+        str: JSON representation of the object
+    """
+    json.dumps(my_obj)
+    return str(my_obj)
