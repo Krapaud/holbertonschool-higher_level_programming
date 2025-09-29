45fc5414e1cfeffb9c6c76705f8290dc446e92b4
Krapaud <mur.mickael@gmail.com>
2025-09-29 15:16:04 +0200

    Add Student class with initialization and JSON serialization methods

---
+++ python-input_output/9-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Module that defines a Student class"""
+
+
+class Student:
+    """
+    A class that defines a student
+    
+    Attributes:
+        first_name (str): The first name of the student
+        last_name (str): The last name of the student
+        age (int): The age of the student
+    """
+    
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiation with first_name, last_name and age
+        
+        Args:
+            first_name (str): The first name of the student
+            last_name (str): The last name of the student
+            age (int): The age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance
+        
+        Returns:
+            dict: Dictionary representation of the Student instance
+        """
+        dict_json_attributes = {}
+        for attribute, value in self.__dict__.items():
+            if type(value) in [list, dict, str, int, bool, float, type(None)]:
+                dict_json_attributes[attribute] = value
+        return dict_json_attributes
