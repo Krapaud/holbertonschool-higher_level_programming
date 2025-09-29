fddd9fa351392bcdb81550192ae3e7883c8925cd
Krapaud <mur.mickael@gmail.com>
2025-09-29 11:06:02 +0200

    Add load_from_json_file function to load objects from JSON files

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that contains a function to load object from JSON file"""
+import json
+
+def load_from_json_file(filename):
+    """
+    Creates an Object from a "JSON file"
+    
+    Args:
+        filename (str): The name of the JSON file to load from
+        
+    Returns:
+        object: The object loaded from the JSON file
+    """
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)
