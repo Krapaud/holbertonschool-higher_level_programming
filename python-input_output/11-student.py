20632083a0aed47aae02c4902ef7cfcdfe680d85
Krapaud <mur.mickael@gmail.com>
2025-09-30 09:28:56 +0200

    Refactor 11-main.py for improved output formatting and add 11-student.py with Student class implementation

---
+++ python-input_output/11-student.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Module that defines a Student class with filter"""
+
+
+class Student:
+    """
+    A class that defines a student
+
+    Attributes:
+        first_name (str): The first name of the student
+        last_name (str): The last name of the student
+        age (int): The age of the student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiation with first_name, last_name and age
+
+        Args:
+            first_name (str): The first name of the student
+            last_name (str): The last name of the student
+            age (int): The age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance
+
+        Args:
+            attrs (list): List of strings, only attribute names contained
+            in this list must be retrieved
+
+        Returns:
+            dict: Dictionary representation of the Student instance
+        """
+        if attrs is None:
+            return self.__dict__
+
+        if isinstance(attrs, list) and all(isinstance(attr, str)
+                                           for attr in attrs):
+            return {attr: getattr(self, attr) for attr in attrs
+                    if hasattr(self, attr)}
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance:"""
+        for keys, value in json.items():
+            setattr(self, keys, value)
