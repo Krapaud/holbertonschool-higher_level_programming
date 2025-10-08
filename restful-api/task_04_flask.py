0a5303c34c87a91bc9dadfc6767cde019c19f78c
Krapaud <mur.mickael@gmail.com>
2025-10-08 10:40:32 +0200

    Remove debug mode and host/port configuration from Flask app run command

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -99,5 +99,4 @@ def add_user():
     return jsonify({"message": "User created", "user": users[username]}), 201
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1', port=5000)
+if __name__ == "__main__": app.run()
