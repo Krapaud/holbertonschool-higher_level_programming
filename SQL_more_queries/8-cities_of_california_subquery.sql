d40b1316988b5fa1a330af86d5e12a0f52e3b2b4
Krapaud <mur.mickael@gmail.com>
2025-10-22 09:36:20 +0200

    Refactor SQL query to use equality instead of IN clause for listing cities in California

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,5 @@
-SELECT id, name
+-- Lists all the cities of California that can be found in the database hbtn_0d_usa
+SELECT name
 FROM cities
-WHERE state_id IN (SELECT id FROM states WHERE name = 'California')
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
