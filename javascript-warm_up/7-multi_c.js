eea5b9d629891a5ab6fba5b3ff45eea83f3a1f9d
Krapaud <mur.mickael@gmail.com>
2025-11-07 11:04:30 +0100

    Add functionality to print 'C is fun' a specified number of times

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const arg = process.argv.slice(2);
+
+const n = parseInt(arg[0]);
+
+if (isNaN(n)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < n; i++) {
+    console.log('C is fun');
+  }
+}
