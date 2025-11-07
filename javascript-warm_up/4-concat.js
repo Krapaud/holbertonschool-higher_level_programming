1f13cb0123e0162c156f3924c2eda933a0da1b06
Krapaud <mur.mickael@gmail.com>
2025-11-07 10:24:40 +0100

    Add argument handling logic to 4-concat.js for improved input validation

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+const args = process.argv.slice(2);
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 2) {
+  console.log(args[0] + ' is ' + args[1]);
+}
+
