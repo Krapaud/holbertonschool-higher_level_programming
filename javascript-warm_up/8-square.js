942e66fbe63256d8c555cc8c58d9c766ed465857
Krapaud <mur.mickael@gmail.com>
2025-11-07 11:13:35 +0100

    Add functionality to print a square of 'X' characters based on user input size

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+const arg = process.argv.slice(2);
+
+const n = parseInt(arg[0]);
+
+if (isNaN(n)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < n; i++) {
+    let line = '';
+    for (let j = 0; j < n; j++) {
+      line += 'X';
+    }
+    console.log(line);
+  }
+}
