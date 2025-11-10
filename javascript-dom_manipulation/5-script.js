70539f638626e20853dfb833cab8a0a8e1bacd1f
Krapaud <mur.mickael@gmail.com>
2025-11-10 10:32:27 +0100

    Add event listener to update header text on button click

--- javascript-dom_manipulation/5-script.js
+++ javascript-dom_manipulation/5-script.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
 // Script to update the text of the header element when clicking on update_header
+document.getElementById('update_header').addEventListener('click', function() {
+    const headerelement = document.querySelector('header');
+    headerelement.textContent = 'New Header!!!';
+});
