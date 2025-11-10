2195ea12a768217a184fbbde45712470a101a745
Krapaud <mur.mickael@gmail.com>
2025-11-10 11:21:30 +0100

    Add functions to fetch and display Star Wars character name and movie titles

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
 // Script to fetch and list all Star Wars movie titles
+async function displayTitle() {
+    const element = document.getElementById('list_movies');
+    const response = await fetch("https://swapi-api.hbtn.io/api/films/?format=json");
+    const data = await response.json();
+    
+    for (const movie of data.results) {
+        const newitem = document.createElement('li');
+        newitem.textContent = movie.title;
+        element.appendChild(newitem);
+    }
+}
+displayTitle();
