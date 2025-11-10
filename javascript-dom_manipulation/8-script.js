52c030af48d301c95b4e69b18f8016ca46fad9ed
Krapaud <mur.mickael@gmail.com>
2025-11-10 12:29:28 +0100

    Add script to fetch and display French translation of "hello"

--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
 // Script to fetch and display a translation of "hello"
+document.addEventListener('DOMContentLoaded', function() {
+async function displayValue() {
+    const element = document.getElementById('hello');
+    const response = await fetch("https://hellosalut.stefanbohacek.com/?lang=fr");
+    const value = await response.json();
+    element.textContent = value.hello;
+}
+displayValue();
+});
