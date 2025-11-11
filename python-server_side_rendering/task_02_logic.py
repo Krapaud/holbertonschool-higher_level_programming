0a385772fe2ce54fc9cee1e670d4b84004b1b571
Krapaud <mur.mickael@gmail.com>
2025-11-11 09:55:37 +0100

    Add initial Flask application and items template for dynamic rendering

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Task 2: Creating a Dynamic Template with Loops and Conditions in Flask
+"""
+
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """Render the home page"""
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    """Render the about page"""
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    """Render the contact page"""
+    return render_template('contact.html')
+
+
+@app.route('/items')
+def items():
+    """Render the items page with data from JSON file"""
+    return render_template('items.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
